129f0379f2bd7526f74b6fe2e9ec0450ee52faa9
Mika <k2763@student.jamk.fi>
2016-04-01 14:15:45 +0300

    palomies liikkuu

---
+++ PalomiesPeli/MainPage.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace PalomiesPeli
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        // butterfly
+        private Palomies palomies;
+        // game loop timer
+        private DispatcherTimer timer;
+        // which keys are pressed
+        
+        private bool RightPressed;
+        private bool LeftPressed;
+
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+
+            ApplicationView.PreferredLaunchWindowingMode
+            = ApplicationViewWindowingMode.PreferredLaunchViewSize;
+            ApplicationView.PreferredLaunchViewSize = new Size(1024, 720);
+
+            // key listeners
+            Window.Current.CoreWindow.KeyDown += CoreWindow_KeyDown;
+            Window.Current.CoreWindow.KeyUp += CoreWindow_KeyUp;
+
+            CreatePalomies();
+            StartGame();
+        }
+
+        private void CoreWindow_KeyUp(CoreWindow sender, KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Left:
+                    LeftPressed = false;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = false;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        private void CoreWindow_KeyDown(CoreWindow sender, KeyEventArgs args)
+        {
+            switch (args.VirtualKey)
+            {
+                case VirtualKey.Left:
+                    LeftPressed = true;
+                    break;
+                case VirtualKey.Right:
+                    RightPressed = true;
+                    break;
+                default:
+                    break;
+
+            }
+        }
+
+        private void CreatePalomies()
+        {
+            palomies = new Palomies
+            {
+                LocationX = MyCanvas.Width / 2 - 75,
+                LocationY = MyCanvas.Height / 2 - 66
+            };
+            // add to canvas
+            MyCanvas.Children.Add(palomies);
+            // show in right location
+            palomies.SetLocation();
+        }
+        private void StartGame()
+        {
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 1000 / 60);
+            timer.Start(); // stop!
+
+        }
+        private void Timer_Tick(object sender, object e)
+        {
+            // move palomies
+            if (LeftPressed) palomies.Move(-1);
+            if (RightPressed) palomies.Move(1);
+            // update palomies location
+            palomies.SetLocation();
+        }
+    }
+}
---
+++ PalomiesPeli/Palomies.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace PalomiesPeli
+{
+    public sealed partial class Palomies : UserControl
+    {
+        // animate butterfly
+        private DispatcherTimer timer;
+        public double LocationX { get; set; }
+        public double LocationY { get; set; }
+        //animate
+        private int currentFrame = 0;
+        private int direction = 1;
+        private int frameHeight = 70;
+        // speed
+        private readonly double MaxSpeed = 10.0;
+        private readonly double Accelerate = 0.5;
+        private double speed;
+        private readonly double Step = 5;
+        public Palomies()
+        {
+            this.InitializeComponent();
+            
+        }
+        private void Animate()
+        {
+            // current frame 0->4, 4->0
+            if (direction == 1) currentFrame++;
+            else currentFrame--;
+            if (currentFrame == 0 || currentFrame == 1)
+                direction *= -1; // 1 tai -1
+            // offset
+            SpriteSheetOffset.Y = currentFrame * -frameHeight;
+        }
+        //animate butterfly
+        /*private void Animate()
+        {
+            timer_tick will be called in every 125 ms
+            timer = new DispatcherTimer();
+            timer.Tick += Timer_Tick;
+            timer.Interval = new TimeSpan(0, 0, 0, 0, 125);
+            timer.Start();
+        }*/
+
+        /*private void Timer_Tick(object sender, object e)
+        {
+            // current frame 0->4, 4->0
+            if (direction == 1) currentFrame++;
+            else currentFrame--;
+            if (currentFrame == 0 || currentFrame == 1)
+                direction *= -1; // 1 tai -1
+            // offset
+            SpriteSheetOffset.Y = currentFrame * -frameHeight;
+
+        }*/
+        // show butterfly in canvas (right location)
+        public void SetLocation()
+        {
+            SetValue(Canvas.LeftProperty, LocationX);
+            SetValue(Canvas.TopProperty, LocationY);
+        }
+        public void Move(int direction)
+        {
+            LocationX = LocationX + Step * direction;
+            SetValue(Canvas.LeftProperty, LocationX);
+            Animate();
+            //more speed
+            //speed += Accelerate;
+            //if (speed > MaxSpeed) speed = MaxSpeed; // max 10
+        }
+    }
+}
